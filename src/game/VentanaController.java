1be2c3988774ceb9e223469dec09fafcb4bbd3cc
eafajardo <eafajardo@poligran.edu.co>
2023-06-29 22:41:36 -0500

    Actualizacion2

--- src/game/VentanaController.java
+++ src/game/VentanaController.java
@@ -1,26 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+/**
+ * Sample Skeleton for 'Ventana.fxml' Controller Class
  */
+
 package game;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author sopor
- */
-public class VentanaController implements Initializable {
+public class VentanaController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btInicio"
+    private Button btInicio; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfedad"
+    private TextField tfedad; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfnombre"
+    private TextField tfnombre; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btInicio != null : "fx:id=\"btInicio\" was not injected: check your FXML file 'Ventana.fxml'.";
+        assert tfedad != null : "fx:id=\"tfedad\" was not injected: check your FXML file 'Ventana.fxml'.";
+        assert tfnombre != null : "fx:id=\"tfnombre\" was not injected: check your FXML file 'Ventana.fxml'.";
+
+    }
+
 }
